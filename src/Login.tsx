3393cce693b0d98133aa14c71f400803f550a871
Ruan <santosruan2021@gmail.com>
2024-04-21 19:22:31 -0300

    feat?: adicionado tailwind css okay e shadow cn ui nada funcional

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,24 +1,23 @@
 import { useState, useRef } from "react";
-// import style from './Login.module.css'
+import { Button } from "../@/components/ui/button";
+import style from "./Login.module.css";
 export function Login() {
   const [loginMode, setLoginMode] = useState(false);
   const loginButtonRef = useRef(null);
 
-  function handleStartLogin() {
-    setLoginMode(true);
-    // loginButtonRef.current.disabled = true;
-  }
-
   return (
-    <div className="border-gray-900 border-8">
-      <header>
-        <h1 className="text-3xl font-bold underline">LABORAS</h1>
+    <div className="bg-[#0F0B1C] min-h-screen ">
+      <header className="border-2 w-full flex justify-center border-red-600">
+        <h1 className="text-3xl text-white font-bold">LABORAS</h1>
       </header>
-      <main>
-        <button ref={loginButtonRef} onClick={handleStartLogin}>
+
+      <main className="min-h-full flex flex-col justify-center items-center   h-4/5 border-2 border-green-600 w-full">
+        <button className="w-fit bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
           Login
         </button>
-        <button>Criar conta</button>
+        <button className="w-fit bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Criar conta
+        </button>
       </main>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5174,
+  },
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-})
+  
+});
