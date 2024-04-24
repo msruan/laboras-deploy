dcc0c11e27aceb69a211cee0f43e4c1af456fb52
Bianca <biancabezerrathe71@gmail.com>
2024-04-23 22:08:30 -0300

    fix: bug da tela branca
    Co-authored-by: Antonio Meireles <Meidesu@users.noreply.github.com>
    Co-authored-by: Ruan <msruan@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.tsx";
-import Login from "./pages/public/LoginPage.tsx";
+import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
+// import Login from "./pages/public/LoginPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <App idLoggedUser={"2"} /> */}
-    <Login/>
-    {/* <RouterProvider router={router}/> */}
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+     <RouterProvider router={router}/> 
+     </ThemeProvider>
   </React.StrictMode>
 );
